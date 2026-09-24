56ebbc2baab05325c516f51b91a09f3fde170a37
martintinix-droid <martintinix@gmail.com>
2026-09-23 21:14:56 -0400

    fixed discrepancies in titles

--- preload/Buildings/Generator/generator.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-@onready var timer: Timer = %Timer
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	timer.timeout.connect(func() ->void:
-		SignalManager.energy_ready.emit())
-		
